4b69f51d4e38822cbf33838da9135c9884960ea7
Devin Riegle <riegledevin@gmail.com>
2019-10-12 15:36:19 -0400

    remove print statemnet

--- lib/src/middleware/init.dart
+++ lib/src/middleware/init.dart
@@ -4,8 +4,6 @@ class Middleware {
   static final String name = 'EXPRESS_INIT';
 
   static init(HttpRequest req, HttpResponse res, Function next) {
-    print('$name middleware');
-
     next();
   }
 }
