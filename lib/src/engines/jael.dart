7a65170fd813572d12902f3d5c4b6a5b099cb59d
Tobe O <thosakwe@gmail.com>
2019-10-13 16:33:46 -0400

    Complete jael render implementaton

--- lib/src/engines/jael.dart
+++ lib/src/engines/jael.dart
@@ -1,24 +1,49 @@
-import 'dart:io';
 import 'package:dart_express/src/engines/engine.dart';
-import 'package:code_buffer/code_buffer.dart' deferred as jael_code_buffer;
-import 'package:jael/jael.dart' deferred as jael;
-import 'package:symbol_table/symbol_table.dart' deferred as jael_symbol_table;
+import 'package:code_buffer/code_buffer.dart';
+import 'package:file/local.dart';
+import 'package:jael/jael.dart' as jael;
+import 'package:jael_preprocessor/jael_preprocessor.dart' as jael;
+import 'package:symbol_table/symbol_table.dart';
 
 class JaelEngine {
   static String ext = '.jael';
 
   static handler(
       String filePath, Map<String, dynamic> options, HandlerCallback callback) {
-    var file = File.fromUri(Uri.file(filePath));
+    var fs = LocalFileSystem();
+    var file = fs.file(filePath);
 
-    file.readAsString().then((str) {
-      var buffer = jael_code_buffer.CodeBuffer();
-      var document = jael.parseDocument(str, sourceUrl: filePath);
-      var scope = jael_symbol_table.SymbolTable(values: options);
+    file.readAsString().then((str) async {
+      // Store any parsing/resolution/rendering errors
+      var errors = <jael.JaelError>[];
 
-      jael.Renderer().render(document, buffer, scope);
+      // Parse the document, of course.
+      var buffer = CodeBuffer();
+      var document =
+          jael.parseDocument(str, sourceUrl: filePath, onError: errors.add);
 
-      callback(null, buffer.toString());
+      // Resolve template includes, etc.
+      document = await jael.resolve(document, fs.directory('views'),
+          onError: errors.add);
+
+      // Render an error page if anything went wrong.
+      if (errors.isNotEmpty) {
+        jael.Renderer.errorDocument(errors, buffer);
+        return callback(null, buffer.toString());
+      }
+
+      // Everything went okay - now render.
+      // In case the rendering throws an error, be prepared to catch and
+      // render it.
+      try {
+        var scope = SymbolTable(values: options);
+        jael.Renderer()
+            .render(document, buffer, scope, strictResolution: false);
+      } on jael.JaelError catch (e) {
+        jael.Renderer.errorDocument([e], buffer..clear());
+        return callback(null, buffer.toString());
+      }
+      return callback(null, buffer.toString());
     }).catchError((e) => callback(e, null));
   }
 
