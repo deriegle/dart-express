b3f61efb15b687337ba8f00951c3c24d053b92d7
Devin Riegle <devin.riegle@joinroot.com>
2020-04-10 17:33:40 -0400

    cleanup

--- test/dart_express_test.dart
+++ test/dart_express_test.dart
@@ -2,15 +2,4 @@
 // import 'package:test/test.dart';
 
 void main() {
-  // group('A group of tests', () {
-  //   Awesome awesome;
-
-  //   setUp(() {
-  //     awesome = Awesome();
-  //   });
-
-  //   test('First Test', () {
-  //     expect(awesome.isAwesome, isTrue);
-  //   });
-  // });
 }
